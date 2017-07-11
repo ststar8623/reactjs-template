e239a881777cf2149944bc016d6d6c40d89b560c
ststar8623 <ststar8623@gmail.com>
2017-07-10 23:02:24 -0700

    Added bootstrap to Home and Header component

---
+++ src/app/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>In a new component</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,26 @@
-console.log('it works');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Header from './components/header';
+import Home from './components/home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById('app'));
